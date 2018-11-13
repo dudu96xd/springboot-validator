9db452a149b3aa5eab4b528675371b15e9197844
Troncoso Salvio <igor.troncoso.salvio@accenture.com>
2018-11-12 22:13:14 -0200

    deletada pasta desnecessaria

--- dump.sql/dump.sql
+++
@@ -1,4 +0,0 @@
-use mydb;
-CREATE TABLE plano(  id_plano int NOT NULL AUTO_INCREMENT,  nome_plano varchar(45),  valor double,  qnt_gigas int,  primary key(id_plano));
-CREATE TABLE usuario (  id_usuario int(11) NOT NULL AUTO_INCREMENT,  nome varchar(45) NOT NULL unique key,  email varchar(45) NOT NULL unique key,  data_ca
-dastro date,  linha varchar(45) NOT NULL unique key,  id_plano int default null,  primary key(id_usuario),  foreign key(id_plano) references plano(id_plano));
